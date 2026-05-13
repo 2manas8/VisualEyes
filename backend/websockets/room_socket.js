48e0142178a8e5bc7bf987f6456b17456dda50a6
Manas Pratim Das <dasmp123@gmail.com>
2026-05-13 20:44:57 +0530

    Socket update to handle directions

--- backend/websockets/room_socket.js
+++ backend/websockets/room_socket.js
@@ -17,46 +17,34 @@ const roomSocket = (socket) => {
         const roomId = data.roomId
         const frame = data.frame
         const objects = data.objects
+        const direction = data.direction
         const audioPath = path.join(__dirname, "../../assets/audio")
         let message = ""
-        let audioPlaylist = []
         if(objects.length === 0) {
             message = "No objects detected"
-            audioPlaylist.push("no_objects.mp3")
         }
         if(objects.length > 5) {
             message = "A lot of objects detected"
-            audioPlaylist.push("a_lot_of_objects.mp3")
         } else {
             const names = [...objects]
             let formattedList = ""
-            const fileNames = objects.map(name => 
-                name.toLowerCase().replace(/ /g, '_') + ".mp3"
-            )
             if (names.length === 1) {
                 formattedList = names[0]
-                audioPlaylist.push(fileNames[0])
             } else {
                 const lastText = names.pop();
                 formattedList = names.join(', ') + " and " + lastText;
-                const lastFile = fileNames.pop();
-                fileNames.forEach(file => audioPlaylist.push(file));
-                audioPlaylist.push("and.mp3");
-                audioPlaylist.push(lastFile);
             }
             message = formattedList.charAt(0).toUpperCase() + formattedList.slice(1) + " detected"
-            audioPlaylist.push("detected.mp3")
         }
-        const audioData = audioPlaylist.map(fileName => {
-            try {
-                const filePath = path.join(audioPath, fileName);
-                const fileBuffer = fs.readFileSync(filePath);
-                return fileBuffer.toString('base64');
-            } catch (err) {
-                console.error(`Error reading audio file ${fileName}:`, err.message);
-                return null; 
-            }
-        }).filter(data => data !== null)
+
+        const audioFile = direction.toLowerCase().replace(/ /g, '_') + ".mp3"
+        const audioData = []
+        try {
+            const fileBuffer = fs.readFileSync(path.join(audioPath, audioFile))
+            audioData.push(fileBuffer.toString('base64'))
+        } catch (err) {
+            console.error(`Error reading audio file ${audioFile}:`, err.message)
+        }
         try {
             Frame.findOneAndUpdate(
                 {roomId: roomId},
